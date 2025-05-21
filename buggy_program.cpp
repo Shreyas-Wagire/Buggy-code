2ea4c1dc0662d1ae7e2860c014f73d67fdece640
tanveer41 <bhatttanveer516@gmail.com>
2025-05-21 11:45:07 +0530

    Fixes: Fixed syntax errors in calculateSquare function

--- buggy_program.cpp
+++ buggy_program.cpp
@@ -1,13 +1,13 @@
-
 #include <iostream>
 using namespace std;
 
-int calculateSquare(int n {
+// Bug fixed by tanveer41 - Fixed function definition and cout operator
+int calculateSquare(int n) {
     return n * n; 
 }
 
 int main() {
     int number = 5;
-    cout >> "Square is: " << calculateSquare(number) << endl;
+    cout << "Square is: " << calculateSquare(number) << endl;
     return 0;
 }
